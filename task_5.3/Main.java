a366d770574caf538b8c3b7d743544d334f9b3e4
vladlen-vozhzhaev <55277829+vladlen-vozhzhaev@users.noreply.github.com>
2020-04-29 10:32:29 +0300

    Create Main.java
    /*Реализовать метод boolean fight(Cat anotherCat):
    * реализовать механизм драки котов в зависимости от их веса, возраста и силы.
    * Нужно сравнить каждый критерий по отдельности, и победителем будет тот, у которого больше "победивших" критериев.
    * Метод должен определять, выиграли ли мы (this) бой или нет, т.е. возвращать true, если выиграли и false - если нет.
    * Если ничья и никто не выиграл, возвращаем либо true либо false, но должно выполняться условие:
    * если cat1.fight(cat2) возвращает true,
    * то cat2.fight(cat1) должен возвращать false. */

---
+++ task_5.3/Main.java
@@ -0,0 +1,28 @@
+/*Реализовать метод boolean fight(Cat anotherCat):
+* реализовать механизм драки котов в зависимости от их веса, возраста и силы.
+* Нужно сравнить каждый критерий по отдельности, и победителем будет тот, у которого больше "победивших" критериев.
+* Метод должен определять, выиграли ли мы (this) бой или нет, т.е. возвращать true, если выиграли и false - если нет.
+* Если ничья и никто не выиграл, возвращаем либо true либо false, но должно выполняться условие:
+* если cat1.fight(cat2) возвращает true,
+* то cat2.fight(cat1) должен возвращать false. */
+public class Main {
+    public static void main(String[] args) {
+      //напишите тут ваш код
+    }
+}
+class Cat {
+    public int age;
+    public int weight;
+    public int strength;
+
+    public Cat() {
+    }
+
+    public boolean fight(Cat anotherCat) {
+        //напишите тут ваш код
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
