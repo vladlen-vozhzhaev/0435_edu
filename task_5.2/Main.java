4d27999da380cc351bb8dc3fa79558ac0e7ee3fe
vladlen-vozhzhaev <55277829+vladlen-vozhzhaev@users.noreply.github.com>
2020-04-29 10:30:20 +0300

    Create Main.java
    /* Создать класс Cat. У кота должно быть имя (name, String), возраст (age, int), вес (weight, int), сила (strength, int).
    * Создайте объект класса Сat*/

---
+++ task_5.2/Main.java
@@ -0,0 +1,10 @@
+/* Создать класс Cat. У кота должно быть имя (name, String), возраст (age, int), вес (weight, int), сила (strength, int).
+* Создайте объект класса Сat*/
+public class Main {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+    }
+}
+public class Cat{
+    //напишите тут ваш код
+}
