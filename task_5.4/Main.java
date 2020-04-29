8ed1001c436241e5d20648a7ee9c97f3bb42df67
vladlen-vozhzhaev <55277829+vladlen-vozhzhaev@users.noreply.github.com>
2020-04-29 10:34:59 +0300

    Create Main.java
    /* Создать class Dog. У собаки должна быть кличка String name и возраст int age.
    * Создайте геттеры и сеттеры для всех переменных класса Dog. 
    * Создайте объект Dog*/

---
+++ task_5.4/Main.java
@@ -0,0 +1,11 @@
+/* Создать class Dog. У собаки должна быть кличка String name и возраст int age.
+* Создайте геттеры и сеттеры для всех переменных класса Dog. 
+* Создайте объект Dog*/
+public class Main {
+    public static void main(String[] args) {
+      //напишите тут ваш код
+    }
+}
+class Dog {
+    //напишите тут ваш код
+}
