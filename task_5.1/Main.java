665bf760d469a72db2e36f44f52aa59804cc3590
vladlen-vozhzhaev <55277829+vladlen-vozhzhaev@users.noreply.github.com>
2020-04-29 10:26:21 +0300

    Create Main.java
    /* Помогите коту обрести имя с помощью метода setName. */

---
+++ task_5.1/Main.java
@@ -0,0 +1,15 @@
+/* Помогите коту обрести имя с помощью метода setName. */
+public class Main {
+    public static void main(String[] args) {
+        Cat cat = new Cat();
+        cat.setName("Жужик");
+        System.out.println(cat.name);
+    }
+}
+public class Cat {
+    private String name = "безымянный кот";
+    public void setName(String name) {
+        //напишите тут ваш код
+
+    }
+}
