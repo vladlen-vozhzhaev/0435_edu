4e917781f51d4dc4ca3e545f793a83f0e5cb9d29
vladlen-vozhzhaev <55277829+vladlen-vozhzhaev@users.noreply.github.com>
2020-05-06 11:24:33 +0300

    Create Main.java

---
+++ task_6.4/Main.java
@@ -0,0 +1,74 @@
+/*Исправь метод containsBones и всю связанную с ним логику так, чтобы:
+1. Поведение программы осталось прежним.
+2. Метод containsBones должен возвращать тип Object и значение "Yes" вместо true, "No" вместо false*/
+
+import java.util.ArrayList;
+import java.util.List;
+
+/* 
+ООП - исправь ошибки в наследовании
+*/
+
+public class Main {
+    public static interface LivingPart {
+        boolean containsBones();
+    }
+
+    public static class BodyPart implements LivingPart {
+        private String name;
+
+        public BodyPart(String name) {
+            this.name = name;
+        }
+
+        public boolean containsBones() {
+            return true;
+        }
+
+        public String toString() {
+            return containsBones() ? name + " содержит кости" : name + " не содержит кости";
+        }
+    }
+
+    public static class Finger extends BodyPart {
+        private boolean isArtificial;
+
+        public Finger(String name, boolean isArtificial) {
+            super(name);
+            this.isArtificial = isArtificial;
+        }
+
+        public boolean containsBones() {
+            return super.containsBones() && !isArtificial;
+        }
+    }
+
+    public static void main(String[] args) {
+        printlnFingers();
+        printlnBodyParts();
+        printlnLivingParts();
+    }
+
+    private static void printlnLivingParts() {
+        System.out.println(new BodyPart("Рука").containsBones());
+    }
+
+    private static void printlnBodyParts() {
+        List<BodyPart> bodyParts = new ArrayList<BodyPart>(5);
+        bodyParts.add(new BodyPart("Рука"));
+        bodyParts.add(new BodyPart("Нога"));
+        bodyParts.add(new BodyPart("Голова"));
+        bodyParts.add(new BodyPart("Тело"));
+        System.out.println(bodyParts.toString());
+    }
+
+    private static void printlnFingers() {
+        List<Finger> fingers = new ArrayList<Finger>(5);
+        fingers.add(new Finger("Большой", true));
+        fingers.add(new Finger("Указательный", true));
+        fingers.add(new Finger("Средний", true));
+        fingers.add(new Finger("Безымянный", false));
+        fingers.add(new Finger("Мизинец", true));
+        System.out.println(fingers.toString());
+    }
+}
