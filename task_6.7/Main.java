735daba0a7be838b7f291a8595a7137781741442
vladlen-vozhzhaev <55277829+vladlen-vozhzhaev@users.noreply.github.com>
2020-05-07 11:58:19 +0300

    Create Main.java

---
+++ task_6.7/Main.java
@@ -0,0 +1,14 @@
+/* Перехватить исключение (и вывести его на экран), указав его тип, возникающее при выполнении кода:
+String s = null;
+String m = s.toLowerCase(); */
+
+public class Main {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+
+        String s = null;
+        String m = s.toLowerCase();
+
+        //напишите тут ваш код
+    }
+}
