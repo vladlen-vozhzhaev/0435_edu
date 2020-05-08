a899a37b94ab6f33d1a8588f31399f93a1c82b53
vladlen-vozhzhaev <55277829+vladlen-vozhzhaev@users.noreply.github.com>
2020-05-08 11:48:33 +0300

    Create Main.java

---
+++ task_7.4/Main.java
@@ -0,0 +1,10 @@
+/*Ввести с клавиатуры дату в формате "2013-08-18"
+Вывести на экран введенную дату в виде "AUG 18, 2013".
+Воспользоваться объектом Date и SimpleDateFormat. */
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        //напишите тут ваш код
+    }
+}
