c1c7ef8164e5fb44985741d401d382f9888f6292
vladlen-vozhzhaev <55277829+vladlen-vozhzhaev@users.noreply.github.com>
2020-05-08 11:49:27 +0300

    Create Main.java

---
+++ task_7.5/Main.java
@@ -0,0 +1,17 @@
+/*Расставьте правильно операторы приведения типа, чтобы получился ответ: d > 0
+int a = 0;
+int b = (byte) a + 46;
+byte c = (byte) (a*b);
+double f = (char) 1234.15;
+long d = (short) (a + f / c + b); */
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 0;
+        int b = (byte) a + 46;
+        byte c = (byte) (a * b);
+        double f = (char) 1234.15;
+        long d = (short) (a + f / c + b);
+        System.out.println(d);
+    }
+}
