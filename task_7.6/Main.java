abead067815510aa205988983f59c27e5f9791ab
vladlen-vozhzhaev <55277829+vladlen-vozhzhaev@users.noreply.github.com>
2020-05-08 11:50:15 +0300

    Create Main.java

---
+++ task_7.6/Main.java
@@ -0,0 +1,17 @@
+/* Расставьте правильно операторы приведения типа, чтобы получился ответ: d=3.765
+int a = 15;
+int b = 4;
+float c = a / b;
+double d = a * 1e-3 + c;*/
+
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 15;
+        int b = 4;
+        float c = a / b;
+        double d = a * 1e-3 + c;
+
+        System.out.println(d);
+    }
+}
