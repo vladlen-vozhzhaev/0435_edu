95aba228324f3c15628dc343adb8062927f554cc
vladlen-vozhzhaev <55277829+vladlen-vozhzhaev@users.noreply.github.com>
2020-05-08 11:41:15 +0300

    Create Main.java

---
+++ task_7.2/Main.java
@@ -0,0 +1,16 @@
+/*
+Программа вводит с клавиатуры имя месяца и выводит его номер на экран в виде: "May is the 5 month".
+Используйте коллекции.
+(Придётся погуглить коллекции)
+*/
+import java.io.IOException;
+
+/* 
+Номер месяца
+*/
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        //напишите тут ваш код
+    }
+}
