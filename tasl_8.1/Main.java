cefe5fcdd9d33ddf4d4f66bb3cd2cd5ce07f28ad
vladlen-vozhzhaev <55277829+vladlen-vozhzhaev@users.noreply.github.com>
2020-05-13 08:40:14 +0300

    Create Main.java

---
+++ tasl_8.1/Main.java
@@ -0,0 +1,33 @@
+/* Задача: Введи с клавиатуры 20 слов и выведи их в алфавитном порядке. Каждое слово - с новой строки. */
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/* 
+Задача по алгоритмам
+*/
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String[] array = new String[20];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = reader.readLine();
+        }
+
+        sort(array);
+
+        for (String word : array) {
+            System.out.println(word);
+        }
+    }
+
+    public static void sort(String[] array) {
+        //напишите тут ваш код
+    }
+
+    //Метод для сравнения строк: 'а' больше чем 'b'
+    public static boolean isGreaterThan(String a, String b) {
+        return a.compareTo(b) > 0;
+    }
+}
